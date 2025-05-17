b9ab6015fc74ba52a1f264a2780af3a9cb9a0a7b
alragi0 <140979427+alragi0@users.noreply.github.com>
2025-05-17 01:54:28 +0100

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(getenv("API_ID", "24140079"))
     API_HASH = getenv("API_HASH", "d4ba07c6bbfd05e8b52dd77880ff254b")
     BOT_TOKEN = getenv("BOT_TOKEN", "6359085597:AAHzb16c7sKbFJCnJSUPIKQZ_CWCojJF3pY")
-    FSUB = getenv("FSUB", "SDBotz")
+    FSUB = getenv("FSUB", "kiiiik")
     CHID = int(getenv("CHID", "-1000112234"))
     SUDO = list(map(int, getenv("6460393623").split()))
     MONGO_URI = getenv("MONGO_URI", "")
